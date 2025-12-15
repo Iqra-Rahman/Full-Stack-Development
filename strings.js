92535c029355ea850ff32ecb6b7ab4fe26174efd
Iqra Rahman <rahmaniqra697@gmail.com>
2025-12-15 20:27:16 +0530

    exercise and calculator

--- strings.js
+++ strings.js
@@ -1,5 +1,20 @@
 var user = "Rama";
-
 var greeting = "Hello! how are you, ";
-
 console.log(greeting + user + "?"); //concatenation of string using + operator
+
+// --------------------------------------------------------
+
+//string always wins when we use + operator with other data types
+
+var points = 100;  //number
+var bonus = "50";  //string 
+let totalPoints = points + bonus;  //number+string=string
+console.log(totalPoints);
+console.log(typeof(totalPoints));
+
+// --------------------------------------------------------
+
+//escape sequences in strings
+// var message = "Hello! i like to watch the movie "Inception""; //SyntaxError: Unexpected identifier 'Inception'
+var message = "Hello! i like to watch the movie \"Inception\"";
+console.log(message);
